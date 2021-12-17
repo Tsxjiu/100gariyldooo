084b58a2770840826f299d7e46eb0428f1359311
Tsxjiu <96261413+Tsxjiu@users.noreply.github.com>
2021-12-17 13:26:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = '100 гармаар байна!'
+let idx = 1
+let speed = 300 / speedEl.value
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+    if (idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(writeText, speed)
+}
+
+
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
